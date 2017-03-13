758f0d5272da2abd80a66986021c3dc43da2d4fa
TekkCraft <marc.wilhelm.ch@gmail.com>
2017-03-13 14:03:57 +0100

    cs

--- src/View/Layout/layout.html.php
+++ src/View/Layout/layout.html.php
@@ -81,56 +81,58 @@
             </ul>
             <div class="float-right">
                 <?php if (empty(session()->get('email')))  : ?>
-                <ul class="hover nav navbar-nav">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span
-                                    class="caret"></span></a>
-                        <ul id="login-dp" class="dropdown-menu">
-                            <li>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <form role="form" method="post" action="<?= $this->e(baseurl('/login')) ?>"
-                                              id="login-nav">
-                                            <div class="form-group">
-                                                <label class="sr-only" for="login_email">Email Adresse</label>
-                                                <input type="email" name="login_email" class="form-control"
-                                                       placeholder="Email Adresse" required>
-                                            </div>
-                                            <div class="form-group">
-                                                <label class="sr-only" for="login_password">Passwort</label>
-                                                <input type="password" name="login_password" class="form-control"
-                                                       placeholder="Passwort" required>
-                                            </div>
-                                            <?php foreach ($this->next('flash') as $type => $messages) : ?>
-                                                <?php foreach ($messages as $message) : ?>
-                                                    <?php if ($type == 'empty') ?>
-                                                        <div class="form-group">
-                                                        <div class="alert alert-danger" role="alert">
-                                                        <span class="help-block"><?php echo $this->e($message); ?></span>
-                                                    </div>
-                                                    </div>
+                    <ul class="hover nav navbar-nav">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span
+                                        class="caret"></span></a>
+                            <ul id="login-dp" class="dropdown-menu">
+                                <li>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <form role="form" method="post" action="<?= $this->e(baseurl('/login')) ?>"
+                                                  id="login-nav">
+                                                <div class="form-group">
+                                                    <label class="sr-only" for="login_email">Email Adresse</label>
+                                                    <input type="email" name="login_email" class="form-control"
+                                                           placeholder="Email Adresse" required>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label class="sr-only" for="login_password">Passwort</label>
+                                                    <input type="password" name="login_password" class="form-control"
+                                                           placeholder="Passwort" required>
+                                                </div>
+                                                <?php foreach ($this->next('flash') as $type => $messages) : ?>
+                                                    <?php foreach ($messages as $message) : ?>
+                                                        <?php if ($type == 'empty') ?>
+                                                            <div class="form-group">
+                                                            <div class="alert alert-danger" role="alert">
+                                                            <span class="help-block"><?php echo $this->e($message); ?></span>
+                                                        </div>
+                                                        </div>
+                                                    <?php endforeach; ?>
                                                 <?php endforeach; ?>
-                                            <?php endforeach; ?>
-                                            <div class="form-group">
+                                                <div class="form-group">
 
-                                            </div>
-                                            <div class="form-group">
-                                                <button type="submit" class="btn btn-primary btn-block">Sign in</button>
-                                            </div>
-                                        </form>
+                                                </div>
+                                                <div class="form-group">
+                                                    <button type="submit" class="btn btn-primary btn-block">Sign in
+                                                    </button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                        <!-- #### Register Button #### -->
+                                        <!--<div class="bottom text-center">-->
+                                        <!--    New here ? <a href="#"><b>Join Us</b></a>-->
+                                        <!--</div>-->
                                     </div>
-                                    <!-- #### Register Button #### -->
-                                    <!--<div class="bottom text-center">-->
-                                    <!--    New here ? <a href="#"><b>Join Us</b></a>-->
-                                    <!--</div>-->
-                                </div>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
                 <?php else : ?>
                     <ul class="nav navbar-nav">
-                        <li><a class="active" href="<?php echo baseurl('/logout') ?>"><?= $this->e(__("Ausloggen")) ?> <span
+                        <li><a class="active" href="<?php echo baseurl('/logout') ?>"><?= $this->e(__("Ausloggen")) ?>
+                                <span
                                         class="sr-only">(current)</span></a>
                         </li>
                     </ul>
